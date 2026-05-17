00d0969859e581a0a80a6aac1ce88aa34ca5c69a
MICHELE MILAZZO <62478661+michelemilazzo@users.noreply.github.com>
2026-05-17 15:58:14 +0200

    feat: register install hook

--- thanatos_intel/hooks.py
+++ thanatos_intel/hooks.py
@@ -1,33 +1,33 @@
-app_name = 'thanatos_intel'
-app_title = 'Thanatos Intel'
-app_version = '0.1.0'
-app_description = 'Intelligence Investigation Platform MVP'
-app_author = 'OneKeyCo'
-app_author_email = 'info@onekeyco.com'
-app_license = 'MIT'
-app_icon = 'icon-bar-chart'
-app_color = 'navy'
-app_email = 'info@onekeyco.com'
-app_docs = 'https://thanatos.onekeyco.com'
-app_country = 'IT'
+app_name='thanatos_intel'
+app_title='Thanatos Intel'
+app_version='0.1.0'
+app_description='Intelligence Investigation Platform MVP'
+app_author='OneKeyCo'
+app_author_email='info@onekeyco.com'
+app_license='MIT'
+app_icon='icon-bar-chart'
+app_color='navy'
+app_email='info@onekeyco.com'
+app_docs='https://thanatos.onekeyco.com'
+app_country='IT'
 
-fixtures = [
-    'Role'
-]
+after_install='thanatos_intel.install.after_install'
 
-doc_events = {}
+fixtures=['Role']
 
-scheduler_events = {
-    'cron': {},
-    'all': [],
-    'daily': [],
-    'daily_long': [],
-    'weekly': [],
-    'weekly_long': [],
-    'monthly': [],
-    'monthly_long': [],
+doc_events={}
+
+scheduler_events={
+ 'cron':{},
+ 'all':[],
+ 'daily':[],
+ 'daily_long':[],
+ 'weekly':[],
+ 'weekly_long':[],
+ 'monthly':[],
+ 'monthly_long':[],
 }
 
-website_route_rules = []
-permission_query_conditions = {}
-has_permission = {}
+website_route_rules=[]
+permission_query_conditions={}
+has_permission={}
