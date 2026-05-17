f3bed15d778245972330b2fe71c5b0311d1baf75
MICHELE MILAZZO <62478661+michelemilazzo@users.noreply.github.com>
2026-05-17 16:00:02 +0200

    feat: connect Thanatos permissions to doctypes

--- thanatos_intel/hooks.py
+++ thanatos_intel/hooks.py
@@ -12,22 +12,17 @@ app_docs='https://thanatos.onekeyco.com'
 app_country='IT'
 
 after_install='thanatos_intel.install.after_install'
-
 fixtures=['Role']
 
 doc_events={}
-
-scheduler_events={
- 'cron':{},
- 'all':[],
- 'daily':[],
- 'daily_long':[],
- 'weekly':[],
- 'weekly_long':[],
- 'monthly':[],
- 'monthly_long':[],
-}
-
+scheduler_events={'cron':{},'all':[],'daily':[],'daily_long':[],'weekly':[],'weekly_long':[],'monthly':[],'monthly_long':[]}
 website_route_rules=[]
 permission_query_conditions={}
-has_permission={}
+has_permission={
+ 'Investigation Case':'thanatos_intel.permissions.can_read_thanatos_doc',
+ 'Investigation Entity':'thanatos_intel.permissions.can_read_thanatos_doc',
+ 'Investigation Evidence':'thanatos_intel.permissions.can_read_thanatos_doc',
+ 'Investigation Report':'thanatos_intel.permissions.can_read_thanatos_doc',
+ 'Risk Score':'thanatos_intel.permissions.can_read_thanatos_doc',
+ 'Chain Of Custody Event':'thanatos_intel.permissions.can_read_thanatos_doc'
+}
