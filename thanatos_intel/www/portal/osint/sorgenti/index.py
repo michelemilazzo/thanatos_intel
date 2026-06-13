6aeb1ad03ba94b0c2f4ac79a5936f051e7f8f867
OneKey MMOS <mic.milazzo@gmail.com>
2026-06-13 11:20:59 +0200

    osint: link Chiavi API visibile solo a System Manager nel recommender

--- thanatos_intel/www/portal/osint/sorgenti/index.py
+++ thanatos_intel/www/portal/osint/sorgenti/index.py
@@ -13,5 +13,6 @@ def get_context(context):
 		raise frappe.Redirect
 	context.no_cache = 1
 	context.target_types = TARGET_TYPES
+	context.is_admin = "System Manager" in frappe.get_roles()
 	from thanatos_intel.osint import source_registry
 	context.overview = source_registry.overview()
