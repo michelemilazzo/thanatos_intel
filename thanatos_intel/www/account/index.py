06fad5ecf7df41e2247cf43ab888b741b12b4bb5
OneKey MMOS <dev@onekeyco.com>
2026-06-20 07:32:56 +0000

    portale: link Sito (nuova finestra)+Account nel nav; scheda /account con KYC/KYB
    - portal_nav: aggiunti Account e Sito (target=_blank) per tornare alla home pubblica
    - /account: card Identita e compliance (tipo cliente, KYC, KYB se societa) da Investigation Client, con CTA a /onboarding/kyc|kyb

--- thanatos_intel/www/account/index.py
+++ thanatos_intel/www/account/index.py
@@ -19,5 +19,15 @@ def get_context(context):
 	u = frappe.get_doc("User", frappe.session.user)
 	context.user_email = u.email
 	context.full_name = u.full_name or u.email
+
+	# Scheda cliente: tipo, KYC e (se societa') KYB
+	cl = frappe.db.get_value(
+		"Investigation Client",
+		{"platform_user": frappe.session.user},
+		["name", "client_type", "kyc_status", "kyb_status", "vat_number", "onboarding_status"],
+		as_dict=True,
+	)
+	context.client = cl
+	context.is_company = bool(cl and (cl.client_type or "") not in ("", "Individual"))
 	context.no_cache = 1
 	return context
