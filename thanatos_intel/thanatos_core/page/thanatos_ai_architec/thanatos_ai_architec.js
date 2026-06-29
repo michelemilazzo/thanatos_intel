01da8e762dc7064dc62a87d0db4d31db4f81e887
OneKey MMOS <dev@onekeyco.com>
2026-06-29 09:55:29 +0000

    Rimuovo desk Page orfana con typo thanatos-ai-architec (residuo non referenziato)

--- thanatos_intel/thanatos_core/page/thanatos_ai_architec/thanatos_ai_architec.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['thanatos-ai-architec'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Architetto AI del caso',
-		single_column: true
-	});
-}
